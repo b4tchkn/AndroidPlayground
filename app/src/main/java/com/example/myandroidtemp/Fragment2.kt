7061e900dbc726b1ae78b4f030a16f0f0cb47308
batch <baaaakkrad@gmail.com>
2019-09-04 22:49:09 +0900

    [add]RecyclerView in Fragment2

--- app/src/main/java/com/example/myandroidtemp/Fragment2.kt
+++ app/src/main/java/com/example/myandroidtemp/Fragment2.kt
@@ -1,10 +1,16 @@
 package com.example.myandroidtemp
 
+import android.icu.text.SimpleDateFormat
 import android.os.Bundle
+import android.os.RecoverySystem
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.list_item.*
+import java.util.*
 
 class Fragment2: Fragment() {
     override fun onCreateView(
@@ -14,4 +20,17 @@ class Fragment2: Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_2, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val date = SimpleDateFormat("yyyy/MM/dd").format(Date())
+        val itemList = mutableListOf<Item>()
+        repeat((0..100).count()) { itemList.add(Item("テスト", date)) }
+
+        view.findViewById<RecyclerView>(R.id.itemRecyclerView).also { recyclerView: RecyclerView ->
+            recyclerView.adapter = ItemViewAdapter(context!!, itemList)
+            recyclerView.layoutManager = LinearLayoutManager(context)
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/myandroidtemp/Item.kt
@@ -0,0 +1,3 @@
+package com.example.myandroidtemp
+
+data class Item(val content: String, val date: String)
