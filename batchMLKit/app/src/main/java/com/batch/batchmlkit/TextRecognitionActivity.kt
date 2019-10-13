18cf72c7c38d86b52f4199fce01d13cd95eb0b8c
batch <baaaakkrad@gmail.com>
2019-10-13 22:26:14 +0900

    [add]Resource files

---
+++ batchMLKit/app/src/main/java/com/batch/batchmlkit/TextRecognitionActivity.kt
@@ -0,0 +1,12 @@
+package com.batch.batchmlkit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TextRecognitionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_text_recognition)
+    }
+}
