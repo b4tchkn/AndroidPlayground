1c9e3c109b363763c3d0b7b3f3314fbc7f94f78e
batch <baaaakkrad@gmail.com>
2019-10-13 17:57:08 +0900

    [add]Create Layout

--- batchMLKit/app/src/main/java/com/batch/batchmlkit/MainActivity.kt
+++ batchMLKit/app/src/main/java/com/batch/batchmlkit/MainActivity.kt
@@ -2,11 +2,24 @@ package com.batch.batchmlkit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun textRecognition(view: View) {
+    }
+
+    fun faceDetection(view: View) {
+    }
+
+    fun barcodeScanner(view: View) {
+    }
+
+    fun imageLabeling(view: View) {
     }
 }
