cc4761c6f3da52f73625329824155e802a893ac0
batch <baaaakkrad@gmail.com>
2020-05-08 14:08:15 +0900

    save

--- RecyclerViewSample/app/build/generated/source/kapt/debug/com/airbnb/epoxy/databinding/BR.java
+++ RecyclerViewSample/app/build/generated/source/kapt/debug/com/airbnb/epoxy/databinding/BR.java
@@ -3,7 +3,7 @@ package com.airbnb.epoxy.databinding;
 public class BR {
   public static final int _all = 0;
 
-  public static final int music = 1;
+  public static final int itemClickListener = 1;
 
-  public static final int itemClickListener = 2;
+  public static final int music = 2;
 }
--- RecyclerViewSample/app/build/generated/source/kapt/debug/com/batch/recyclerviewsample/BR.java
+++ RecyclerViewSample/app/build/generated/source/kapt/debug/com/batch/recyclerviewsample/BR.java
@@ -3,7 +3,7 @@ package com.batch.recyclerviewsample;
 public class BR {
   public static final int _all = 0;
 
-  public static final int music = 1;
+  public static final int itemClickListener = 1;
 
-  public static final int itemClickListener = 2;
+  public static final int music = 2;
 }
