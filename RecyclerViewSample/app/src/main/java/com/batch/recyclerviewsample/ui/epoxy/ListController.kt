c6cb5682af7b1f4ab5a60fd7f85bb68906d233a4
batch <baaaakkrad@gmail.com>
2020-06-18 20:25:57 +0900

    add carousel in Epoxy

--- RecyclerViewSample/app/src/main/java/com/batch/recyclerviewsample/ui/epoxy/ListController.kt
+++ RecyclerViewSample/app/src/main/java/com/batch/recyclerviewsample/ui/epoxy/ListController.kt
@@ -1,11 +1,14 @@
 package com.batch.recyclerviewsample.ui.epoxy
 
 import android.view.View
+import com.airbnb.epoxy.CarouselModel_
 import com.airbnb.epoxy.TypedEpoxyController
 import com.airbnb.epoxy.carousel
+import com.batch.recyclerviewsample.ItemBindingModel_
 import com.batch.recyclerviewsample.headerView
 import com.batch.recyclerviewsample.item
 import com.batch.recyclerviewsample.model.Music
+import com.batch.recyclerviewsample.ui.groupie.ListItem
 
 class ListController(private val callback: ClickListener) : TypedEpoxyController<List<Music>>() {
     interface ClickListener {
@@ -24,13 +27,22 @@ class ListController(private val callback: ClickListener) : TypedEpoxyController
                 itemClickListener(View.OnClickListener { callback.itemClickListener(music) })
             }
         }
-//        carousel {
-//            data.forEach { music ->
-//                item {
-//                    id(0)
-//                    music(music)
-//                }
-//            }
-//        }
+
+
+        val itemModels = data.map { item ->
+            ItemBindingModel_()
+                .id(0)
+                .music(item)
+        }
+
+        CarouselModel_()
+            .id("carousel")
+            .models(itemModels)
+            .addTo(this)
+
+        carousel {
+            id("carousel extension")
+            models(itemModels)
+        }
     }
 }
