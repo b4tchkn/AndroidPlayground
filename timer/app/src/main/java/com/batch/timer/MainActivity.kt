29f716f60cf8c915d5f2a869a0d020a5f6389674
batch <baaaakkrad@gmail.com>
2020-08-10 21:02:44 +0900

    add timeToText

--- timer/app/src/main/java/com/batch/timer/MainActivity.kt
+++ timer/app/src/main/java/com/batch/timer/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.min
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         val runnable = object : Runnable {
             override fun run() {
                 timeCount++
-                text_timer.text = timeCount.toString()
+                text_timer.text = timeToText(timeCount)
                 // 1秒間隔でDelayを発生させてrunさせる
                 handler.postDelayed(this, 1000)
             }
@@ -37,6 +38,20 @@ class MainActivity : AppCompatActivity() {
             if (isRunning) {
                 handler.removeCallbacks(runnable)
                 isRunning = false
+            }
+        }
+    }
+
+    private fun timeToText(time: Int): String {
+        return when (time) {
+            0 -> {
+                "00:00:00"
+            }
+            else -> {
+                val hour = time / 3600
+                val minute = time % 3600 / 60
+                val second = time % 60
+                "%1$02d:%2$02d:%3$02d".format(hour, minute, second)
             }
         }
     }
