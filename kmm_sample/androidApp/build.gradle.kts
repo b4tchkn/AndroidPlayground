815210e8a8c740434fb1558e7fcfcf0b61fe018b
batch <baaaakkrad@gmail.com>
2020-12-31 09:10:36 +0900

    add dependencies in androidApp

--- kmm_sample/androidApp/build.gradle.kts
+++ kmm_sample/androidApp/build.gradle.kts
@@ -8,6 +8,11 @@ dependencies {
     implementation("com.google.android.material:material:1.2.1")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    implementation("androidx.core:core-ktx:1.3.2")
+    implementation("androidx.recyclerview:recyclerview:1.1.0")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation("androidx.cardview:cardview:1.0.0")
 }
 
 android {
