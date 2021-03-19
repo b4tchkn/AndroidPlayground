e75ece36c6fb7648f10c087824ad3d92297c67ab
batch <baaaakkrad@gmail.com>
2021-03-20 02:58:51 +0900

    save InstagramHome

--- composepractice/app/src/main/java/com/batch/compose_practice/ui/instagram_home/InstagramHomeViewModel.kt
+++ composepractice/app/src/main/java/com/batch/compose_practice/ui/instagram_home/InstagramHomeViewModel.kt
@@ -4,9 +4,7 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-class InstagramHomeViewModel(
-
-) : ViewModel() {
+class InstagramHomeViewModel : ViewModel() {
 
     private val _posts = MutableStateFlow<List<Post>>(listOf())
     val posts: StateFlow<List<Post>>
