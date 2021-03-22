972f3fcf5eb4d27e580cf48d91fca7b7c2dc87a4
batch <baaaakkrad@gmail.com>
2021-03-22 23:19:57 +0900

    fix layout in Instagram Story

--- composepractice/app/src/main/java/com/batch/compose_practice/ui/instagram_home/InstagramHomeScreen.kt
+++ composepractice/app/src/main/java/com/batch/compose_practice/ui/instagram_home/InstagramHomeScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.batch.compose_practice.R
@@ -36,6 +38,15 @@ fun InstagramHomeScreen(navController: NavController) {
     ) {
         viewModel.getPosts()
         LazyColumn {
+            item {
+                InstagramHomeStorySection()
+            }
+            item {
+                Divider(
+                    color = colorResource(id = R.color.lightGrey),
+                    thickness = 0.5.dp
+                )
+            }
             items(posts) { post ->
                 InstagramHomePostItem(post = post)
             }
---
+++ composepractice/app/src/main/java/com/batch/compose_practice/ui/instagram_home/InstagramHomeStorySection.kt
@@ -0,0 +1,55 @@
+package com.batch.compose_practice.ui.instagram_home
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.batch.compose_practice.R
+import com.batch.compose_practice.ui.theme.instagramGradient
+
+@Composable
+fun InstagramHomeStorySection() {
+    LazyRow(Modifier.padding(vertical = 8.dp)) {
+        items(10) {
+            Box(modifier = Modifier.padding(horizontal = 4.dp)) {
+                Image(
+                    painter = painterResource(id = R.drawable.account_icon_image_zoo),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(72.dp)
+                        .clip(CircleShape)
+                        .border(
+                            shape = CircleShape,
+                            border = BorderStroke(
+                                width = 2.dp,
+                                brush = Brush.linearGradient(
+                                    colors = instagramGradient,
+                                    start = Offset(
+                                        0f,
+                                        0f
+                                    ),
+                                    end = Offset(
+                                        100f,
+                                        100f
+                                    )
+                                )
+                            )
+                        )
+                )
+            }
+        }
+    }
+}
