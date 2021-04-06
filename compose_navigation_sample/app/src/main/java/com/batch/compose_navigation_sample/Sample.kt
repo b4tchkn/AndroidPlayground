16708c88b7de9daca93ae6870b8aa94c3b1decee
batch <baaaakkrad@gmail.com>
2021-04-07 02:22:22 +0900

    impl

---
+++ compose_navigation_sample/app/src/main/java/com/batch/compose_navigation_sample/Sample.kt
@@ -0,0 +1,8 @@
+package com.batch.compose_navigation_sample
+
+import java.io.Serializable
+
+data class Sample(
+    val id: Int,
+    val content: String,
+): Serializable
--- compose_navigation_sample/app/src/main/java/com/batch/compose_navigation_sample/SecondActivity.kt
+++ compose_navigation_sample/app/src/main/java/com/batch/compose_navigation_sample/SecondActivity.kt
@@ -1,5 +1,6 @@
 package com.batch.compose_navigation_sample
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
@@ -16,41 +17,42 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.batch.compose_navigation_sample.ui.theme.ComposenavigationsampleTheme
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposenavigationsampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = Color.White) {
+                Surface(color = Color.Blue) {
                     Column(
                         modifier = Modifier
-                            .fillMaxHeight()
-                            .fillMaxWidth(),
+                            .fillMaxWidth()
+                            .fillMaxHeight(),
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally,
                     ) {
-                        Text(text = "This is MainActivity")
-                        NextButton(onClickButton = ::goToSecondActivity)
+                        Text(text = "This is SecondActivity")
+                        Text(text = getExtras().toString())
+                        BackButton(onClickButton = { finish() })
                     }
                 }
             }
         }
     }
 
-    private fun goToSecondActivity() {
-        startActivity(
-            SecondActivity.createIntent(
-                this,
-                Sample(id = 1, content = "This is content.")
-            )
-        )
+    private fun getExtras(): Sample = intent.getSerializableExtra("sample_key") as Sample
+
+    companion object {
+        fun createIntent(activity: AppCompatActivity, sample: Sample): Intent {
+            return Intent(activity, SecondActivity::class.java).apply {
+                putExtra("sample_key", sample)
+            }
+        }
     }
 }
 
 @Composable
-fun NextButton(onClickButton: () -> Unit) {
+fun BackButton(onClickButton: () -> Unit) {
     Button(onClick = onClickButton) {
-        Text(text = "Next")
+        Text(text = "Back")
     }
 }
