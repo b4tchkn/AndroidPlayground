f6ddb00fa5aeb2ab53047f891f4885ed3ccd50d0
b4tchkn <baaaakkrad@gmail.com>
2021-07-03 13:59:38 +0900

    feat: add AndroidView TextView Sample

--- composepractice/app/src/main/java/com/batch/compose_practice/ui/android_view_sample/AndroidViewSampleScreen.kt
+++ composepractice/app/src/main/java/com/batch/compose_practice/ui/android_view_sample/AndroidViewSampleScreen.kt
@@ -1,15 +1,20 @@
 package com.batch.compose_practice.ui.android_view_sample
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import android.widget.TextView
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
 import com.batch.compose_practice.R
 
@@ -33,14 +38,33 @@ fun AndroidViewSampleScreen(navController: NavController) {
             )
         }
     ) {
+        val context = LocalContext.current
+
+        val customTextView = remember {
+            TextView(context).apply {
+                text = "これはrememberで定義した変数をAndroidViewに注入したTextViewです"
+                setTextAppearance(R.style.Teal200_H1_Bold)
+            }
+        }
+
         Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(),
+                .fillMaxSize()
+                .background(Color.White),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text(text = "hogeeee")
+            Text(text = "これはComposableTextです", color = Color.Green)
+            AndroidView(
+                factory = { context ->
+                    TextView(context).apply {
+                        text = "これはAndroidViewで直接WrapしたTextViewです"
+                        setTextAppearance(R.style.Purple200_H1_Bold)
+                    }
+                },
+            )
+
+            AndroidView({ customTextView })
         }
     }
 }
