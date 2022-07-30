fbc8697c2a15e1b88b190a6928b6102e0e58e1f7
b4tchkn <baaaakkrad@gmail.com>
2022-07-30 15:56:12 +0900

    refactor: move ui to page

--- ComposeApp/app/src/main/java/com/example/composeapp/ui/page/SamplePage.kt
+++ ComposeApp/app/src/main/java/com/example/composeapp/ui/page/SamplePage.kt
@@ -1,8 +1,15 @@
 package com.example.composeapp.ui.page
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import com.example.composeapp.model.Sample2Model
+import com.example.composeapp.model.SampleModel
 
 @Composable
-fun SamplePage() {
-
+fun SamplePage(sample: SampleModel, sample2: Sample2Model) {
+    Column {
+        Text(text = sample.toString())
+        Text(text = sample2.toString())
+    }
 }
--- ComposeApp/app/src/main/java/com/example/composeapp/ui/page/SamplePageCoordinator.kt
+++ ComposeApp/app/src/main/java/com/example/composeapp/ui/page/SamplePageCoordinator.kt
@@ -1,15 +1,13 @@
 package com.example.composeapp.ui.page
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.composeapp.state.SampleState
 import com.example.composeapp.state.Sample2State
+import com.example.composeapp.state.SampleState
 import com.example.composeapp.ui.Coordinator
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -26,10 +24,10 @@ fun SamplePageCoordinator(
         },
     ) {
         Coordinator(states = listOf(sampleState.state, sampleState2.state)) {
-            Column {
-                Text(text = sampleState.state.value?.data.toString())
-                Text(text = sampleState2.state.value?.data.toString())
-            }
+            SamplePage(
+                sample = sampleState.state.value!!.data!!,
+                sample2 = sampleState2.state.value!!.data!!,
+            )
         }
     }
 }
